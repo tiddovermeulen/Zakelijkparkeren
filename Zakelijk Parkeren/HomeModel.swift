904e0aaba044c0042c7729c55419f856726b9785
Tiddo Vermeulen <tiddovermeulen@tiddos-imac.fritz.box>
2017-01-17 16:05:05 +0100

    .

---
+++ Zakelijk Parkeren/HomeModel.swift
@@ -0,0 +1,56 @@
+//
+//  HomeModel.swift
+//  Zakelijk Parkeren
+//
+//  Created by Tiddo Vermeulen on 15/01/2017.
+//  Copyright © 2017 Tiddo Vermeulen. All rights reserved.
+//
+
+import Foundation
+
+protocol HomeModelProtocal: class {
+    func itemsDownloaded(items: NSArray)
+}
+
+
+class HomeModel: NSObject, NSURLSessionDataDelegate {
+    
+    //properties
+    
+    weak var delegate: HomeModelProtocal!
+    
+    var data : NSMutableData = NSMutableData()
+    
+    let urlPath: String = "http://iosquiz.com/service.php" //this will be changed to the path where service.php lives
+    
+    
+    func downloadItems() {
+        
+        let url: NSURL = NSURL(string: urlPath)!
+        var session: NSURLSession!
+        let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        
+        
+        session = NSURLSession(configuration: configuration, delegate: self, delegateQueue: nil)
+        
+        let task = session.dataTaskWithURL(url)
+        
+        task.resume()
+        
+    }
+    
+    func URLSession(session: NSURLSession, dataTask: NSURLSessionDataTask, didReceiveData data: NSData) {
+        self.data.appendData(data);
+        
+    }
+    
+    func URLSession(session: NSURLSession, task: NSURLSessionTask, didCompleteWithError error: NSError?) {
+        if error != nil {
+            print("Failed to download data")
+        }else {
+            print("Data downloaded")
+            self.parseJSON()
+        }
+        
+    }
+}
---
+++ Zakelijk Parkeren/LocationModel.swift
@@ -0,0 +1,95 @@
+//
+//  LocationModel.swift
+//  Zakelijk Parkeren
+//
+//  Created by Tiddo Vermeulen on 15/01/2017.
+//  Copyright © 2017 Tiddo Vermeulen. All rights reserved.
+//
+
+import Foundation
+
+class LocationModel: NSObject {
+    
+    //properties
+    
+    var name: String?
+    var address: String?
+    var latitude: String?
+    var longitude: String?
+    
+    
+    //empty constructor
+    
+    override init()
+    {
+        
+    }
+    
+    //construct with @name, @address, @latitude, and @longitude parameters
+    
+    init(name: String, address: String, latitude: String, longitude: String) {
+        
+        self.name = name
+        self.address = address
+        self.latitude = latitude
+        self.longitude = longitude
+        
+    }
+    
+    
+    //prints object's current state
+    
+    override var description: String {
+        return "Name: \(name), Address: \(address), Latitude: \(latitude), Longitude: \(longitude)"
+        
+    }
+    func parseJSON() {
+        
+        var jsonResult: NSMutableArray = NSMutableArray()
+        
+        do{
+            jsonResult = try JSONSerialization.JSONObjectWithData(self.data, options:JSONSerialization.ReadingOptions.AllowFragments) as! NSMutableArray
+            
+        } catch let error as NSError {
+            print(error)
+            
+    
+        }
+        
+        var jsonElement: NSDictionary = NSDictionary()
+        let locations: NSMutableArray = NSMutableArray()
+        
+        for(var i = 0; i < jsonResult.count; i += 1)
+        {
+            
+            jsonElement = jsonResult[i] as! NSDictionary
+            
+            let location = LocationModel()
+            
+            //the following insures none of the JsonElement values are nil through optional binding
+            if let name = jsonElement["Name"] as? String,
+                let address = jsonElement["Address"] as? String,
+                let latitude = jsonElement["Latitude"] as? String,
+                let longitude = jsonElement["Longitude"] as? String
+            {
+                
+                location.name = name
+                location.address = address
+                location.latitude = latitude
+                location.longitude = longitude
+                
+            }
+            
+            locations.addObject(location)
+            
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            
+            self.delegate.itemsDownloaded(locations)
+            
+        })
+    }
+    
+    
+}
